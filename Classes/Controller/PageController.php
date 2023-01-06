b8fb176db8778902f7c52057684ea300cc9a892f
Xertiler <58569236+Xertiler@users.noreply.github.com>
2023-01-06 18:34:10 +0100

    Refactored Files according to Typo3 conventions

--- Classes/Controller/PageController.php
+++ Classes/Controller/PageController.php
@@ -5,13 +5,16 @@ declare(strict_types=1);
 namespace Gruppe1\Stylingcockpit\Controller;
 
 
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
 /**
  * This file is part of the "Styling Cockpit" Extension for TYPO3 CMS.
  *
  * For the full copyright and license information, please read the
  * LICENSE.txt file that was distributed with this source code.
  *
- * (c) 2022 
+ * (c) 2022
  */
 
 
@@ -41,36 +44,15 @@ class PageController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
      * @return \Psr\Http\Message\ResponseInterface
      */
     public function indexAction(): \Psr\Http\Message\ResponseInterface{
-        $PageTSconfig = \TYPO3\CMS\Backend\Utility\BackendUtility::getPagesTSconfig($this->pObj->id);
-        $websiteID = $PageTSconfig['TSFE.']['constants.']['websiteConfig.'];
-
-        foreach ($PageTSconfig['TSFE'] as $dogshit){
-            echo $dogshit;
-        }
-
-        //$currentPid = $GLOBALS['TSFE']->id;
-        //$this->view->assign('pageID', [$currentPid]);
-        //echo 'this is a test' + $websiteID;
-        //echo $this->request->getBaseUri();
-        //echo 'PHP version: ' . phpversion();
-
         return $this->htmlResponse();
     }
 
-    public function testAction(){
-        //$PageTSconfig = \TYPO3\CMS\Backend\Utility\BackendUtility::getPagesTSconfig($this->pObj->id);
-        //$websiteID = $PageTSconfig['TSFE.']['constants.']['websiteConfig.']['id'];
-        echo 'test ausgabe';
-    }
 
-    public function anotherTestAction(){
-        echo "test2 ausgabe";
-        //http_redirect('http://www.google.com');
-    }
+    public function doSomethingAction(ServerRequestInterface $request): ResponseInterface
+    {
+        $data = ['result' => 'pls work'];
 
-
-    public function ajaxTest(){
-        echo "hello world";
+        return $this->jsonResponse(json_encode($data));
     }
 
 
